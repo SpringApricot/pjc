79e6b195d44e71c759871ae7979c8bbcaa3191ef
Joanna <azia54@gmail.com>
2020-01-25 23:49:35 +0100

    Uploading zad5.

--- labyPJC/kolos/kolos.cpp
+++ labyPJC/kolos/kolos.cpp
@@ -52,6 +52,18 @@ struct Rect {
     Rect(Point dl, Point gp) : dl(dl), gp(gp) {};
 };
 
+struct Segment {
+    double a, b; // a <= b
+
+    Segment(double a, double b) : a(a), b(b) {};
+};
+
+bool intersect(const Segment* ps, const Segment& ra) {
+    return ((ps->a < ra.a) && (ps->b > ra.a))
+        || ((ps->a < ra.b) && (ps->b > ra.b))
+        || ((ps->a > ra.a) && (ps->b < ra.b));
+}
+
 //Funkcje do rozwiazan
 int fun(const Node* head) {
     const Node* current = head;
@@ -90,10 +102,44 @@ void check4() {
     std::cout << perim(rect) << std::endl;
 }
 
+void check5() {
+    Segment a = Segment(2, 4);
+    Segment b = Segment(5, 7);
+    Segment c = Segment(9, 11);
+    Segment d = Segment(13, 15);
+    Segment e = Segment(17, 19);
+    Segment ver = Segment(6, 14);
+
+    bool test1 = (false == intersect(&ver, a));
+    bool test2 = (true == intersect(&ver, b));
+    bool test3 = (true == intersect(&ver, c));
+    bool test4 = (true == intersect(&ver, d));
+    bool test5 = (false == intersect(&ver, e));
+
+    bool test1b = (false == intersect(&a, ver));
+    bool test2b = (true == intersect(&b, ver));
+    bool test3b = (true == intersect(&c, ver));
+    bool test4b = (true == intersect(&d, ver));
+    bool test5b = (false == intersect(&e, ver));
+
+    if (test1 && test2 && test3 && test4 && test5) std::cout << "Success!" << std::endl;
+    else std::cout << "Error!" << std::endl;
+
+    if (test1b && test2b && test3b && test4b && test5b) std::cout << "Success 2!" << std::endl;
+    else {
+        std::cout << "Error 2!" << std::endl;
+        std::cout << "1: " << test1b << std::endl;
+        std::cout << "2: " << test2b << std::endl;
+        std::cout << "3: " << test3b << std::endl;
+        std::cout << "4: " << test4b << std::endl;
+        std::cout << "5: " << test5b << std::endl;
+    }
+}
+
 //Main '\_:)_/' <- rip unicode
 int main()
 {
-    check4();
+    check5();
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
